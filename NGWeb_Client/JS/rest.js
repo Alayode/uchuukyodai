f58cb8675729c8ab389bb2865c7275967acea357
KayodeAlayode <ksamuel.chris@icloud.com>
2014-01-21 18:48:17 -0500

    Created functions to show the NOTE read fromt he JSON objects or from the REST
    *****
    remember @param {type} note note object

--- NGWeb_Client/JS/rest.js
+++ NGWeb_Client/JS/rest.js
@@ -4,10 +4,9 @@
 /**
  * Load shit onto dong ready
  * @author: Chris Samuel <ksamuel.chris@icloud.com>
- *
+ * var note = new log
  * */
 
-        // var note = new log
 
     var modalWindows = "static/html/modal_windows.html";
     var templates = "static/html/templates.html";
@@ -57,13 +56,96 @@
         startListeningForNoteClicks();
     });
 
-}
+
 
     function loadNotesAutomatically(){
         //Automatically load new notes when at the end of the page
         $('load_notes').scrol(function(e){
             var scrollDiv = $('#load_notes');
 
-          // console
-        })
+          // console.log(scrollDiv.prop('scrollHeight') + "-" +scrollDiv.scrollTop() + " - " + scrollDiv.height());
+
+            if(scrollDiv.prop('scrollHeight') - scrollDiv.scrollTop() <= scrollDiv.height()){
+                page = page + 1;
+                console.log('increate to ' + page)
+                LoadNote(page);
+            }
+        });
+    }
+
+    /**
+     * Get log from json object or from REST if it does not exist
+     * @param {type} id note id
+     *
+     * */
+
+    function getNote(id){
+
+        //Load Note
+        if(id in savedNotes){
+            showNote(savedNOtes[id]);
+
+
+        }else{
+            $.getJSON(serviceurl + 'logs/' + id, function(log){
+                showNote(note);
+            });
+        }
+    }
+
+
+    /***
+     * Show note that was read from JSON object or from REST
+     * @param {type} note note object
+     *
+     * */
+
+
+    function showNote(log){
+        $('.container-right').show("fast");
+
+        $("note_description").html(note.description);
+        $("#note_owner").html(note.owner);
+        $("#note_date").html(formatDate(note.createdDate));
+
+        //Show date edited
+        if(log.createdDate !== log.lastModifiedDate){
+            var template = getTemplate("template_note_details_edited");
+
+            var item = {
+                editedDate: formatDate(log.modifiedDate)
+
+            };
+
+            var html = Mustache.to_html(template, item);
+
+            $('#note_details_edited').html(html);
+
+
+        } else {
+            $('#log_details_edited').html("");
+
+        }
+
+
+        //Load note notebooks
+        $("load_note_notebooks").html("");
+
+        if(note.tags.length !== 0){
+            repeat("template_note_tag", "load_note_tags", note , "notes");
+        }
+
+
+        //Load attachments
+        $('#load_note_attachments').html("");
+
+        if(note.attachments.length!== 0 ){
+            $('.note_attachments').show("fast");
+            repeatAttachments("templates_note_attachment", "load_note_attachments", note.attachments, note.id);
+
+        } else {
+            $('.note_attachments').hide("fast");
+        }
+
+
     }
