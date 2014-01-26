e0dff90bf2aa9f888747058158ce7d4d3e298a78
KayodeAlayode <ksamuel.chris@icloud.com>
2014-01-26 17:10:40 -0500

    added search.js added  functions for general Search Query

---
+++ NGWeb_Client/JS/new_log.js
@@ -0,0 +1,92 @@
+/*
+ * Script specific to edit_log.html
+ *
+ * @author: Dejan Dežman <dejan.dezman@cosylab.com>
+ */
+
+
+$(document).ready(function(){
+
+	jQuery.expr[':'].Contains = function(a, i, m){
+		return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+	};
+
+	/**
+	 * Disable closing the login dropdown if user clicks on login form elements
+	 */
+	// Setup drop down menu
+	$('.dropdown-toggle').dropdown();
+	// Fix input element click problem
+	$('.dropdown-menu form').click(function(e) {
+		e.stopPropagation();
+	});
+
+	$('#new_log').click(function(e){
+		window.location.href = "new_log.html";
+	});
+
+	// Load Logbooks
+	loadLogbooks();
+
+	// Load Tags
+	loadTags();
+
+	// Wait for dataload
+	$('#load_tags').on('dataloaded', function(e){
+		autocompleteTags(savedTags);
+	});
+
+	// Wait for dataload
+	$('#load_logbooks').on('dataloaded', function(e){
+		autocompleteLogbooks(savedLogbooks);
+	});
+
+	// Add upload field
+	addAttachmentField();
+
+	$('#add_attachment').click(function(e){
+		addAttachmentField();
+	});
+});
+
+function addAttachmentField(){
+	var template = getTemplate("template_new_add_attachment");
+
+	var addAtachment = {};
+
+	var html = Mustache.to_html(template, addAtachment);
+	$('#list_add_attachments').append(html);
+
+	$('.new_attachment').unbind("change");
+	$('.new_attachment').on("change", function(e){
+		alert($(e.target).val());
+	});
+}
+
+function autocompleteTags(tagsArray){
+	var selectedTagsArray = [];
+
+	if($.cookie(filtersCookieName) !== undefined){
+		selectedTagsArray = $.parseJSON($.cookie(filtersCookieName))["list2"];
+	}
+
+	$("#tags_input").tagsManager({
+		prefilled: selectedTagsArray,
+		typeahead: true,
+		typeaheadSource: tagsArray
+	});
+}
+
+function autocompleteLogbooks(logbooksArray){
+	var selectedLogbooksArray = [];
+
+	if($.cookie(filtersCookieName) !== undefined){
+		selectedLogbooksArray = $.parseJSON($.cookie(filtersCookieName))["list"];
+	}
+
+	$("#logbooks_input").tagsManager({
+		prefilled: selectedLogbooksArray,
+		typeahead: true,
+		typeaheadSource: logbooksArray
+	});
+}
--- NGWeb_Client/JS/search.js
+++ NGWeb_Client/JS/search.js
@@ -1,3 +1,119 @@
 /**
  * Created by azuramei on 1/24/14.
+ * Using jQuery to load data
  */
+
+
+/**
+ *
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ *
+ * **/
+
+$(document.ready(function(){
+    //wait for the data to load
+    $('#load_notebooks').on('dataSelected',function(e,data){
+        //parseSearchQuery();
+        generateSearchQuery(data);
+    });
+
+    //wait for the data to load then load the tags
+    $('#load_tags').on('dataSelected', function(e, data){
+        //parseSearchQuery();
+        generateSearchQuery(data);
+    });
+});
+
+/*
+* Start listening for Search button clicks. When button is pressed, parse input and create search
+* query @returns {undefined}
+*
+*/
+
+ function activateSearch(){
+     //Simple search
+     $("search-button").click(function(e){
+         var searchQuery = $("search-query").val();
+
+         if(search === ""){
+             page = 1;
+             loadLogs(page);
+
+         } else {
+             searchForNotes('search=' + searchQuery);
+         }
+     });
+ }
+ function searchForNotes(searchQuery){
+     searchQuery = serviceurl + 'notes?' + searchQuery + 'limit=' + numberofNotesPerLoad + '&page=' + page;
+     console.log(searchQuery);
+
+     //Load Notes
+     $.getJson(searchQuery, function(logs) {
+         $(".note-last").remove();
+         $(".note").remove();
+         repeatNotes("template_note","load_notes",notes);
+         startListeningForLogClicks();
+
+     });
+ }
+
+function buildSearchLanguage(value){
+
+    var searchString = "";
+    var filterType   = "";
+    var remainder    = "";
+
+    var re = new RegExp("(tag:| logbook:| from:|to:)(.*)", "i");
+    var searchParts = re.exec(value);
+
+    if(searchParts === null){
+        searchString = value;
+
+    } else {
+        filterType = searchParts[1];
+        searchString = value.split(filterType)[0];
+        remainder = searchParts[2];
+    }
+
+    return [searchString, filterType, remainder];
+
+
+}
+
+function parseSearchQuery(){
+    var value = $("search-query").val();
+
+    var parsedStringParts = buildSearchLanguage(value);
+    console.log(parsedStringParts);
+
+     while (parsedStringParts[1] !== ""){
+         parsedStringsParts = buildSearchLanguage(parsedStringParts[2]);
+         //console.log(parsedStringParts);
+
+     }
+}
+
+
+/**
+ * Generate search input string and search query
+ * @param {type} dataArray currently selected notebooks and tags
+ * @returns {undefined}
+ *  */
+
+function generateSearchQuery(dataArray){
+    var value = $("#search-query").val();
+    var queryString = "";
+
+    var parsedStringParts = buildSearchLanguage(value);
+
+    var newValue = parsedStringParts[0];
+
+    //Append general search to a search query
+    if(newValue  !== ""){
+        queryString += "search=" + newValue + '&';
+
+    }
+}
+
